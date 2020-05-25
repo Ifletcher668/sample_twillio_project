7822d3fa8c63bbbcc0306ca093ad4bb4e9351078
IFletcher <Isiahfletcher7@gmail.com>
2020-05-25 16:46:08 -0600

    Refactored server

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const http = require("http");
 const express = require("express");
-const twilio = require("twilio");
-const MessagingResponse = twilio.twiml.MessagingResponse;
+const cors = require("cors");
+const port = 1337;
 
 const accountSid = require("./api_secrets").accountSid;
 const authToken = require("./api_secrets").authToken;
-
 const client = require("twilio")(accountSid, authToken);
-const cors = require("cors");
-const port = 1337;
+
 const app = express();
 app.use(
     cors(),
@@ -19,35 +17,20 @@ app.use(
 );
 
 app.post("/sms", (req, res) => {
-    const twiml = new MessagingResponse();
-    console.log("request");
-    console.log(req.body.phoneNumberInput);
-
-    let fourDigitNumber = "";
+    let randomFourDigitStringNumber = "";
     for (let i = 0; i < 4; i++) {
-        fourDigitNumber += Math.floor(Math.random() * 10);
+        randomFourDigitStringNumber += Math.floor(Math.random() * 10);
     }
-
     client.messages
         .create({
-            body: `Your 4-digit code: ${fourDigitNumber}`,
+            body: `Your 4-digit code: ${randomFourDigitStringNumber}`,
             from: "+12083286691",
             to: req.body.phoneNumberInput,
         })
-        .then((message) => console.log(message.sid));
-
-    // twiml.message();
-    res.send(fourDigitNumber);
-    res.writeHead(200, { "content-Type": "text/xml" });
-    res.end(twiml.toString());
+        .then((message) => console.log(message));
+    res.send(randomFourDigitStringNumber);
 });
-
-app.get("/sms", (req, res) => {});
 
 http.createServer(app).listen(port, () =>
     console.log(`Express server listening on port ${port}`)
 );
-
-// const AllRoutes = require("./server/routes/productManager.routes");
-
-// AllRoutes(app);
